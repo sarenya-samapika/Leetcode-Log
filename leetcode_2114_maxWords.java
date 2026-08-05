2a759bd6fb4b89cd5e341403b0b34221ab4f42c0
sarenya-samapika <sarenyasamapika@gmail.com>
2026-08-06 00:31:56 +0530

    leetcode_2114_maxWords.java

---
+++ leetcode_2114_maxWords.java
@@ -0,0 +1,17 @@
+public class leetcode_2114_maxWords {
+    public int mostWordsFound(String[] sentences) {
+        int max =0;
+        for(int i = 0; i<sentences.length; i++){
+            int words = 1;
+            for (int j = 0; j<sentences[i].length(); j++){
+                if(sentences[i].charAt(j) == ' '){
+                    words++;
+                }
+            }
+            if (words > max){
+                max = words;
+            }
+        }
+        return max;
+    }
+}
