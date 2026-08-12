77c3626470d26fa79891d6ef41294f9e60f3f3de
sarenya-samapika <sarenyasamapika@gmail.com>
2026-08-12 11:58:29 +0530

    leetcode_69_squareRoot.java

---
+++ leetcode_69_squareRoot.java
@@ -0,0 +1,16 @@
+public class leetcode_69_squareRoot {
+    public int mySqrt(int x) {
+        int start = 1;
+        int end = x;
+        while(start <= end ){
+            int mid = start + (end - start) / 2;
+            if(mid > x/mid){
+                end = mid -1 ;
+            }
+            else {
+                start = mid +1;
+            }
+        }
+        return end;
+    }
+}
